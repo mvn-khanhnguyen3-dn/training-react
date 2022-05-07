4ec2f46aefd6049f870b43f07d419605f468ab14
Khanh <nguyenquockhanhkz20@gmail.com>
2022-05-07 15:36:25 +0700

    update

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -4,20 +4,15 @@ import {useHistory} from 'react-router-dom'
 
 export default function Account() {
   let history = useHistory();
-  let {login,logout} = useAuth();
-
-  return login ? (
-    <p className="container auth flex align-items-center">
+  let {logout} = useAuth();
+  return (
+   <>
+    <div className="container auth flex align-items-center">
       <h3 className="auth-title">Welcome</h3>
-      <button className="btn"
-        onClick={() => {
-          logout(() => history.push("/"));
-        }}
-      >
+      <button className="btn" onClick={() => {logout()}}>
         Sign out
       </button>
-    </p>
-  ) : (
-    <p className="container auth">You are not logged in.</p>
-  );
+    </div>
+   </>
+  )
 }
--- src/pages/Auth/Login/index.jsx
+++ src/pages/Auth/Login/index.jsx
@@ -5,17 +5,20 @@ export default function Login() {
   const [email,setEmail] = useState('nguyenquockhanhkz20@gmail.com')
   const [password,setPassword] = useState(123)
   let {login} = useAuth()
+  const loginEvent = (e) =>{
+    e.preventDefault();
+    login({email},{password})
+  }
   return (
    <>
     <div className="container flex align-items-center auth">
     <form className="form">
+    <h3 className="auth-title">Page Login</h3>
     <input placeholder='Email' onChange={(e)=> setEmail(e.target.value)} type="text" />
     <input placeholder='Password' onChange={(e)=> setPassword(e.target.value)} type="password" />
+    <button onClick={(e) => loginEvent(e)} className="btn">Login</button>
     </form>
-    <h3 className="auth-title">Page Login</h3>
-    <button onClick={() => login({email},{password})} className="btn">Login</button>
     </div>
-    
    </>
   )
 }
